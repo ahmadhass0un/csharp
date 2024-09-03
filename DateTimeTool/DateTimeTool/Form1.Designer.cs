024a6484a6c0102b5f1194a09825500aeeb21472
ahmadhass0un <vgh4.mggg@outlook.com>
2024-09-03 20:10:18 +0300

    DateTimePicker

--- DateTimeTool/DateTimeTool/Form1.Designer.cs
+++ DateTimeTool/DateTimeTool/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dateTimePicker1 = new DateTimePicker();
+            buttonGO2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 23);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(526, 51);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // buttonGO2
+            // 
+            buttonGO2.Location = new Point(12, 341);
+            buttonGO2.Name = "buttonGO2";
+            buttonGO2.Size = new Size(246, 109);
+            buttonGO2.TabIndex = 1;
+            buttonGO2.Text = "Go 2";
+            buttonGO2.UseVisualStyleBackColor = true;
+            buttonGO2.Click += buttonGO2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 101);
+            button1.Name = "button1";
+            button1.Size = new Size(246, 109);
+            button1.TabIndex = 2;
+            button1.Text = "Go 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(390, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 46);
+            label1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(12, 263);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(526, 51);
+            dateTimePicker2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(390, 373);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 46);
+            label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(18F, 45F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1335, 590);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(buttonGO2);
+            Controls.Add(dateTimePicker1);
+            Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(7);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Button buttonGO2;
+        private Button button1;
+        private Label label1;
+        private DateTimePicker dateTimePicker2;
+        private Label label2;
     }
 }
--- DateTimeTool/DateTimeTool/Form1.cs
+++ DateTimeTool/DateTimeTool/Form1.cs
@@ -6,5 +6,21 @@ namespace DateTimeTool
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = dateTimePicker2.Value.ToString();
+
+        }
+
+        private void buttonGO2_Click(object sender, EventArgs e)
+        {
+            label2.Text = dateTimePicker2.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker2.Value = DateTime.Now;
+        }
     }
 }
